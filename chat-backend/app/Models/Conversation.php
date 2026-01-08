bb6a3550456b24950093d27d664ea9a05466072f
Darshan KC <darshanbahadurchhetri@gmail.com>
2026-01-08 22:31:15 +0545

    feat: conversation model

--- chat-backend/app/Models/Conversation.php
+++ chat-backend/app/Models/Conversation.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Conversation extends Model
 {
-    public $fillable = ['type', 'created_by', 'created_at', 'last_message_id', 'last_message_at'];
+    protected $fillable = [
+        'type',
+        'created_by',
+        'last_message_id',
+        'last_message_at',
+    ];
+
+    public function participants()
+    {
+        return $this->hasMany(ConversationParticipant::class);
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function lastMessage()
+    {
+        return $this->belongsTo(Message::class, 'last_message_id');
+    }
 }
