03794f67e45788f05e768e2466a66b9be13ac60e
Darshan KC <darshanbahadurchhetri@gmail.com>
2026-01-10 22:17:00 +0545

    feat: message model

--- chat-backend/app/Models/Message.php
+++ chat-backend/app/Models/Message.php
@@ -1,10 +1,32 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Message extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'conversation_id',
+        'sender_id',
+        'type',
+        'body',
+        'edited_at',
+    ];
+
+    protected $casts = [
+        'edited_at' => 'datetime',
+    ];
+
+    public function conversation()
+    {
+        return $this->belongsTo(Conversation::class);
+    }
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
 }
