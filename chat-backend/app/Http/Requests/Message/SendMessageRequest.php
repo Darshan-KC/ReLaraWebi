49a28673fa620dbe7b71a9e5340e682b0b3535fe
Darshan KC <darshanbahadurchhetri@gmail.com>
2026-01-12 19:39:02 +0545

    feat:HTTP Layer

---
+++ chat-backend/app/Http/Requests/Message/SendMessageRequest.php
@@ -0,0 +1,20 @@
+namespace App\Http\Requests\Message;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SendMessageRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'conversation_id' => ['required', 'exists:conversations,id'],
+            'body' => ['required', 'string'],
+            'type' => ['nullable', 'in:text,image,system'],
+        ];
+    }
+}
