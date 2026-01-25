e3dd8ae907a6757d3fbca9ee6a085c49c30d2e83
Darshan KC <darshanbahadurchhetri@gmail.com>
2026-01-25 21:15:51 +0545

    feat: conversation read controller

--- chat-backend/app/Http/Controllers/Api/ConversationReadController.php
+++ chat-backend/app/Http/Controllers/Api/ConversationReadController.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Actions\Conversation\MarkConversationReadAction;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Conversation;
+use Illuminate\Support\Facades\Auth;
 
 class ConversationReadController extends Controller
 {
-    //
+    public function store(
+        Conversation $conversation,
+        MarkConversationReadAction $action
+    ) {
+        $action->execute(
+            conversation: $conversation,
+            userId: Auth::id()
+        );
+
+
+        return response()->noContent();
+    }
 }
