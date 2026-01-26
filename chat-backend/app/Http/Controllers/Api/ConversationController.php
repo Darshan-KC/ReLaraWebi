a7e7c39c84f9cceec72e710f8010b9d41d99611c
Darshan KC <darshanbahadurchhetri@gmail.com>
2026-01-26 21:15:17 +0545

    feat: api conversation controller

--- chat-backend/app/Http/Controllers/Api/ConversationController.php
+++ chat-backend/app/Http/Controllers/Api/ConversationController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Actions\Conversation\GetUserConversationsAction;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ConversationResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ConversationController extends Controller
 {
-    //
+    public function index(GetUserConversationsAction $action)
+    {
+        return ConversationResource::collection(
+            $action->execute(Auth::id())
+        );
+    }
 }
