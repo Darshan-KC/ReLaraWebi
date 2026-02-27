48f3fdacf8feb5ae7905db7165012562894a6e89
Darshan KC <darshanbahadurchhetri@gmail.com>
2026-02-27 19:57:43 +0545

    feat: login page state

--- chat-frontend/src/pages/Login.jsx
+++ chat-frontend/src/pages/Login.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  }
   return (
     <div className="flex items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Login Page</h1>
-      <form className="flex flex-col gap-4 mt-8">
+      <form className="flex flex-col gap-4 mt-8" onChange={handleChange}>
         <input
           type="email"
+          name="email"
           placeholder="Email"
           className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
         <input
           type="password"
+          name="password"
           placeholder="Password"
           className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
