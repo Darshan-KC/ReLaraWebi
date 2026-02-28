65f6a282c8a153c956edb6f60bf7b47acc85aabf
Darshan KC <darshanbahadurchhetri@gmail.com>
2026-02-28 21:02:54 +0545

    feat: handle submit

--- chat-frontend/src/pages/Login.jsx
+++ chat-frontend/src/pages/Login.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
 
 export default function Login() {
+  const {Login} = useAuth();
+  // const navigate = useNavigate();
   const [form, setForm] = useState({
     email: "",
     password: "",
@@ -12,20 +15,28 @@ export default function Login() {
       [e.target.name]: e.target.value,
     });
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    Login(form);
+    // navigate("/chat");
+  }
   return (
     <div className="flex items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Login Page</h1>
-      <form className="flex flex-col gap-4 mt-8" onChange={handleChange}>
+      <form className="flex flex-col gap-4 mt-8" onSubmit={handleSubmit}>
         <input
           type="email"
           name="email"
           placeholder="Email"
+          onChange={handleChange}
           className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
         <input
           type="password"
           name="password"
           placeholder="Password"
+          onChange={handleChange}
           className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
         <button
