a46f3693f73eb07faf302d56b1c3ec3f7f3d4160
Darshan KC <darshanbahadurchhetri@gmail.com>
2026-03-03 21:43:54 +0545

    feat: Register page

---
+++ chat-frontend/src/pages/Register.jsx
@@ -0,0 +1,8 @@
+export default function register()
+{
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold">Register Page</h1>
+        </div>
+    );
+}
