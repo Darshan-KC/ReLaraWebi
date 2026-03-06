f9333016547711f2bc5c24eab36ebe37c945789c
Darshan KC <darshanbahadurchhetri@gmail.com>
2026-03-06 20:39:57 +0545

    feat: auth user

--- chat-backend/app/Http/Controllers/Api/AuthController.php
+++ chat-backend/app/Http/Controllers/Api/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -12,7 +13,10 @@ class AuthController extends Controller
      */
     public function index()
     {
-        //
+        $user = Auth::user();
+        return response()->json([
+            'user' => $user,
+        ]);
     }
 
     /**
