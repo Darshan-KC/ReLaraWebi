fb63992a3da547e826f4c42fb7b5f113b6ef6456
Darshan KC <darshanbahadurchhetri@gmail.com>
2026-03-13 20:54:34 +0545

    fix: login page

--- chat-frontend/src/pages/Login.jsx
+++ chat-frontend/src/pages/Login.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-  const {Login} = useAuth();
+
+  const { login } = useAuth();
   const navigate = useNavigate();
+
   const [form, setForm] = useState({
     email: "",
     password: "",
@@ -15,40 +17,43 @@ export default function Login() {
       ...form,
       [e.target.name]: e.target.value,
     });
-  }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     try {
-      await Login(form);
+      await login(form);
       navigate("/");
     } catch (error) {
       console.error("Login failed:", error);
     }
-    // Login(form);
-    // navigate("/chat");
-  }
+  };
+
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Login Page</h1>
+
       <form className="flex flex-col gap-4 mt-8" onSubmit={handleSubmit}>
         <input
           type="email"
           name="email"
           placeholder="Email"
           onChange={handleChange}
-          className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="px-4 py-2 border rounded-md"
         />
+
         <input
           type="password"
           name="password"
           placeholder="Password"
           onChange={handleChange}
-          className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="px-4 py-2 border rounded-md"
         />
+
         <button
           type="submit"
-          className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors duration-300"
+          className="px-4 py-2 bg-blue-500 text-white rounded-md"
         >
           Login
         </button>
