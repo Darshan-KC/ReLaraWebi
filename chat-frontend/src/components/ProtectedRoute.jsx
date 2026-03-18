af7219e147368a4d41e32713ed90615966387a2e
Darshan KC <darshanbahadurchhetri@gmail.com>
2026-03-18 21:16:55 +0545

    feat: protected route

---
+++ chat-frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+
+export default function ProtectedRoute({ children }) {
+    const { user, loading } = useAuth();
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (!user) {
+        return <Navigate to="/login" />;
+    }
+
+    return children;
+}
