3184fc5da17ae4dbf0078050941a1739120260f9
Darshan KC <darshanbahadurchhetri@gmail.com>
2026-03-19 21:59:07 +0545

    feat: router

--- chat-frontend/src/App.jsx
+++ chat-frontend/src/App.jsx
@@ -44,35 +44,56 @@ function App() {
     }
   }
 
+
   return (
-    <div>
-      <h1>Chat App</h1>
+    <>
+      <Routes>
 
-      <div
-        style={{
-          border: '1px solid #ccc',
-          height: '300px',
-          overflowY: 'scroll',
-          padding: '10px'
-        }}
-      >
-        {messages.map((msg, index) => (
-          <div key={index}>{msg.body}</div>
-        ))}
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+      </Routes>
+
+
+      <div>
+        <h1>Chat App</h1>
+
+        <div
+          style={{
+            border: '1px solid #ccc',
+            height: '300px',
+            overflowY: 'scroll',
+            padding: '10px'
+          }}
+        >
+          {messages.map((msg, index) => (
+            <div key={index}>{msg.body}</div>
+          ))}
+        </div>
+
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Type a message..."
+          style={{ width: '70%', padding: '5px' }}
+        />
+
+        <button onClick={sendMessage} style={{ padding: '5px 10px' }}>
+          Send
+        </button>
       </div>
-
-      <input
-        type="text"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        placeholder="Type a message..."
-        style={{ width: '70%', padding: '5px' }}
-      />
-
-      <button onClick={sendMessage} style={{ padding: '5px 10px' }}>
-        Send
-      </button>
-    </div>
+    </>
   )
 }
 
