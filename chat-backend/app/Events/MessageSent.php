f526af3fc1a175528d2bc93b485ab209cf4c07f1
Darshan KC <darshanbahadurchhetri@gmail.com>
2026-05-09 22:00:46 +0545

    fix: message sent event

--- chat-backend/app/Events/MessageSent.php
+++ chat-backend/app/Events/MessageSent.php
@@ -32,4 +32,15 @@ class MessageSent implements ShouldBroadcast
     {
         return 'message.sent';
     }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'id' => $this->message->id,
+            'message' => $this->message->message,
+            'sender_id' => $this->message->sender_id,
+            'receiver_id' => $this->message->receiver_id,
+            'created_at' => $this->message->created_at,
+        ];
+    }
 }
