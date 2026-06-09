1bcbdc1ef1ecb853a5b30aa2d14ea5e0fa431ea5
Darshan KC <darshanbahadurchhetri@gmail.com>
2026-06-09 21:03:23 +0545

    feat: accept friend request

--- chat-frontend/src/services/friendship.service.js
+++ chat-frontend/src/services/friendship.service.js
@@ -2,4 +2,10 @@ import api from './api';
 
 export const sendFriendRequest = async (receiverId) => {
     const response = await api.post('/friend-requests', { receiverId });
+    return response.data;
+}
+
+export const acceptFriendRequest = async (requestId) => {
+    const response = await api.post(`/friend-requests/${requestId}/accept`);
+    return response.data;
 }
