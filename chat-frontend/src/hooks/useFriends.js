42ff1c6a7812b1ee2c3def48ade936068c43aea2
Darshan KC <darshanbahadurchhetri@gmail.com>
2026-06-13 21:24:13 +0545

    feat: states in friends hook

--- chat-frontend/src/hooks/useFriends.js
+++ chat-frontend/src/hooks/useFriends.js
@@ -1,7 +1,37 @@
 import { useEffect, useState } from "react";
+import {
+  getUsers,
+  sendFriendRequest,
+  getFriendRequests,
+  acceptFriendRequest,
+  getFriends,
+} from "../services/friendService";
 
 export default function useFriends() {
     const [users, setUsers] = useState([]);
+  const [requests, setRequests] = useState([]);
+  const [friends, setFriends] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
+  // Load all data
+  const loadData = async () => {
+    setLoading(true);
+
+    try {
+      const [u, r, f] = await Promise.all([
+        getUsers(),
+        getFriendRequests(),
+        getFriends(),
+      ]);
+
+      setUsers(u);
+      setRequests(r);
+      setFriends(f);
+    } finally {
+      setLoading(false);
+    }
+  };
     return ;
 
 }
