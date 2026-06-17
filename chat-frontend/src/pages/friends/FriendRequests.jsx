726dd96232f20509ab4880c23f9b0aaae8656e98
Darshan KC <darshanbahadurchhetri@gmail.com>
2026-06-17 21:45:06 +0545

    feat: Friend request page

---
+++ chat-frontend/src/pages/friends/FriendRequests.jsx
@@ -0,0 +1,10 @@
+export default function FriendRequests() {
+
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-4">
+        Friend Requests
+      </h1>
+      </div>
+      );
+}
