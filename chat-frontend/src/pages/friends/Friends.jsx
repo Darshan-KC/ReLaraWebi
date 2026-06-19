81b54a7d9b8013cd4171ba2e4b38ff1ac5fec40a
Darshan KC <darshanbahadurchhetri@gmail.com>
2026-06-19 20:29:50 +0545

    feat: friends page

---
+++ chat-frontend/src/pages/friends/Friends.jsx
@@ -0,0 +1,9 @@
+export default function Friend() {
+    return (
+        <div className="p-4">
+            <h1 className="text-xl font-bold mb-4">
+                My Friends
+            </h1>
+        </div>
+    );
+}
