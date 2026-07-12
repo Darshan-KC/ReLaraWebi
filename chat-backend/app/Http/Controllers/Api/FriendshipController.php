752c7cbf69e3c7afac32d78ef7acf4b4479ff4ad
Darshan KC <darshanbahadurchhetri@gmail.com>
2026-07-12 20:51:37 +0545

    feat: friendship controller

--- chat-backend/app/Http/Controllers/Api/FriendshipController.php
+++ chat-backend/app/Http/Controllers/Api/FriendshipController.php
@@ -13,6 +13,16 @@ use App\Models\Friendship;
 
 class FriendshipController extends Controller
 {
+    public function users(Request $request)
+    {
+        $user = $request->user();
+
+        $users = \App\Models\User::where('id', '!=', $user->id)->get();
+
+        return response()->json([
+            'data' => $users,
+        ]);
+    }
     public function store(
         SendFriendRequest $request,
         SendFriendRequestAction $action
@@ -48,6 +58,48 @@ class FriendshipController extends Controller
             'data' => FriendshipResource::make(
                 $friendship
             ),
+        ]);
+    }
+
+    public function listRequests(Request $request)
+    {
+        $user = $request->user();
+
+        $friendRequests = Friendship::where('recipient_id', $user->id)
+            ->where('status', 'pending')
+            ->get();
+
+        return response()->json([
+            'data' => FriendshipResource::collection($friendRequests),
+        ]);
+    }
+
+    public function listFriends(Request $request)
+    {
+        $user = $request->user();
+
+        $friends = Friendship::where(function ($query) use ($user) {
+            $query->where('sender_id', $user->id)
+                ->orWhere('recipient_id', $user->id);
+        })
+            ->where('status', 'accepted')
+            ->get();
+
+        return response()->json([
+            'data' => FriendshipResource::collection($friends),
+        ]);
+    }
+
+    public function listSentRequests(Request $request)
+    {
+        $user = $request->user();
+
+        $sentRequests = Friendship::where('sender_id', $user->id)
+            ->where('status', 'pending')
+            ->get();
+
+        return response()->json([
+            'data' => FriendshipResource::collection($sentRequests),
         ]);
     }
 }
