4b15d61d4498b90332e764dc636699e8a1c7ad9a
Darshan KC <darshanbahadurchhetri@gmail.com>
2026-07-29 21:40:20 +0545

    feat: accept request

--- chat-backend/app/Actions/Friendship/AcceptFriendRequestAction.php
+++ chat-backend/app/Actions/Friendship/AcceptFriendRequestAction.php
@@ -4,6 +4,7 @@ namespace App\Actions\Friendship;
 
 use App\Models\Conversation;
 use App\Models\Friendship;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class AcceptFriendRequestAction
@@ -11,19 +12,35 @@ class AcceptFriendRequestAction
     public function execute(
         Friendship $friendship
     ): array {
-
         $conversation = DB::transaction(function () use ($friendship) {
 
             $friendship->update([
                 'status' => 'accepted',
-                'accepted_at' => now(),
+                'accepted_at' => now()
             ]);
 
-            $conversation = Conversation::create();
+            $conversation = Conversation::create([
+                'type' => 'private',
+                'created_by' => Auth::id(),
+            ]);
 
-            $conversation->participants()->attach([
-                $friendship->sender_id,
-                $friendship->receiver_id,
+            // $conversation->participants()->attach([
+            //     $friendship->sender_id,
+            //     $friendship->receiver_id,
+            // ]);
+            $conversation->participants()->createMany([
+                [
+                    'user_id' => $friendship->sender_id,
+                    'role' => 'member',
+                    'joined_at' => now(),
+                    'is_active' => true,
+                ],
+                [
+                    'user_id' => $friendship->receiver_id,
+                    'role' => 'member',
+                    'joined_at' => now(),
+                    'is_active' => true,
+                ],
             ]);
 
             return $conversation;
